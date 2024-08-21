6558ad85ad4d4b36946ead59e2f87f7a0352e9c6
Arseniy2059 <semyashulkovy64@mail.ru>
2024-08-22 00:29:16 +0300

    Add files via upload

---
+++ jk.py
@@ -0,0 +1,13 @@
+first = 10
+second = 10
+third = 10
+
+if first == second != third or first == third != second or second == third != first:
+    print(2)
+elif first == second == third:
+    print(3)
+else:
+    print(0)
+
+
+
