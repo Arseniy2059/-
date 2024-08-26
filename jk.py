5f6119b245e4b3315d7e50b65e207f2dc030fffc
Arseniy2059 <semyashulkovy64@mail.ru>
2024-08-26 22:41:31 +0300

    Update jk.py

--- jk.py
+++ jk.py
@@ -1,10 +1,11 @@
+name = input("Введие число ")
 first = 10
 second = 10
 third = 10
 
-if first == second != third or first == third != second or second == third != first:
+if first == second == third:
     print(2)
-elif first == second == third:
+elif first == second != third or first == third != second or second == third != first:
     print(3)
 else:
     print(0)
